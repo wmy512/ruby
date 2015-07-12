c1e85857bbc5258dcd5c80616c03425aafa51251
mingyinw <wmy512@gmail.com>
2015-07-12 06:42:01 -0700

    chapter 13: integration test

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
+  resources :orders
   resources :line_items
   resources :carts
   get 'store/index'
 
   resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'store#index', as: 'index'
+  root 'store#index', as: 'store'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
